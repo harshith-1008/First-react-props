310c798ff7407fa10ce37179e738a2605dd31634
harshith-1008 <saiharshith.poli@gmail.com>
2023-12-14 13:44:32 +0530

    nice

--- src/Content.js
+++ src/Content.js
@@ -7,7 +7,7 @@ export default function Content(){
             <div>
             <img src={pic} alt="player" className="w-3/5"/>
             <div className="absolute right-0 bottom-0 text-black bg-white w-2/4 p-12">
-                <h2 className="text-4xl font-semibold ">THE LEADER IN INTERACTIVE VR</h2>
+                <h2 className="text-4xl font-semibold ">THE LEADER IN INTERACTIVE VR.</h2>
                 <p className="mt-6 text- font-medium text-gray-400">Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
             </div>
             </div>
