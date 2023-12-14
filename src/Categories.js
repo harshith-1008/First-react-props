6e1486345758b44cd3dc371b1df511ce6e9f9bde
harshith-1008 <123820448+harshith-1008@users.noreply.github.com>
2023-12-14 21:40:47 +0530

    Update Categories.js

--- src/Categories.js
+++ src/Categories.js
@@ -5,7 +5,7 @@ export default function Categories(props){
     return(
         <div className="flex flex-col"> 
                 <div className="flex mb-6 mr-6 relative">
-                    <img className="" src={props.img}/>
+                    <img className="" src={props.img} alt="tmkc"/>
                     <div className="absolute bottom-0 p-10 hover:bg-black">
                         <h3 className="text-4xl text-light text-white">{props.title}</h3>
                     </div>
